660f8a0d8b1566f176df21d9cb745862d7646943
youyoung00 <you889@hanmail.net>
2022-01-19 10:49:24 +0900

    PostRepositoryImpl 클래스 구현.

--- lib/data/data_repository/post_repository_impl.dart
+++ lib/data/data_repository/post_repository_impl.dart
@@ -1,16 +1,20 @@
+import 'package:flutter_clean_architecture/data/data_source/remote/post_remote_data_source.dart';
 import 'package:flutter_clean_architecture/domain/domain_model/post.dart';
 import 'package:flutter_clean_architecture/domain/domain_repository/post_repository.dart';
 
 class PostRepositoryImpl implements PostRepository {
+  final PostRemoteDataSource _dataSource;
+  final int pageLimit = 10;
+
+  PostRepositoryImpl(this._dataSource);
+
   @override
-  Future<List<Post>> getAllPosts() {
-    // TODO: implement getAllPosts
-    throw UnimplementedError();
+  Future<List<Post>> getAllPosts() async {
+    return await _dataSource.getAllPosts();
   }
 
   @override
-  Future<List<Post>> getPost(int page) {
-    // TODO: implement getPost
-    throw UnimplementedError();
+  Future<List<Post>> getPost(int page) async {
+    return await _dataSource.getPosts(page, pageLimit);
   }
 }
